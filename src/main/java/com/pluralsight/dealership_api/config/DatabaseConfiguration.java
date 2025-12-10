cad809d0f578cb6ea85b3ab3a4f16afaab48a6a8
eriinys <eriinys@duck.com>
2025-12-10 13:45:59 -0600

    Ported datasource configuration from previous Workshop 8w into application.properties and DatabaseConfiguration

---
+++ src/main/java/com/pluralsight/dealership_api/config/DatabaseConfiguration.java
@@ -0,0 +1,31 @@
+package com.pluralsight.dealership_api.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DatabaseConfiguration {
+    private BasicDataSource basicDataSource;
+
+    @Autowired
+    public DatabaseConfiguration(@Value("${datasource.url}") String url,
+                                 @Value("${datasource.username}")String user,
+                                 @Value("${datasource.password}")String pass){
+        basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(url);
+        basicDataSource.setUsername(user);
+        basicDataSource.setPassword(pass);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        return basicDataSource;
+    }
+}
